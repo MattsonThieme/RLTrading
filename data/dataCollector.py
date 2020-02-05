9f2d2302cfac15b3779ac503b318ff7b43773436
mattsonthieme <mattson.thieme@gmail.com>
2020-02-04 19:33:44 -0600

    Update datacollector

--- data/dataCollector.py
+++ data/dataCollector.py
@@ -5,13 +5,19 @@ import csv
 
 exchange = ccxt.binance ()
 
-eth = exchange.fetch_ticker('ETH/USDT')
-del eth['info']
-del eth['datetime']
-del eth['timestamp']
+symbol = 'ETH/USDT'
+
+eth = exchange.fetch_ticker(symbol)
+
+to_delete = ['average', 'percentage', 'change', 'previousClose', 'info',
+             'datetime', 'timestamp', 'symbol', 'baseVolume', 'quoteVolume']
+
+for i in to_delete:
+    del eth[i]
+
 write = ['time']
 
-with open('highres.csv', 'w') as r:
+with open('ETH_1s.csv', 'w') as r:
     reader = csv.reader(r)
     try:
         row1 = next(reader)
@@ -21,27 +27,22 @@ with open('highres.csv', 'w') as r:
         writer = csv.writer(r)
         writer.writerow(write)
 
-print(write)
-
-
 delay = 1
 
 while True:
 
     write = []
 
-
     now = datetime.datetime.utcnow()
-    eth = exchange.fetch_ticker('ETH/USDT')
-    del eth['info']
-    del eth['datetime']
-    del eth['timestamp']
+    eth = exchange.fetch_ticker(symbol)
+    for i in to_delete:
+        del eth[i]
 
     write.append((now.hour*3600)+(now.minute*60)+now.second)
     for i in eth:
         write.append(eth[i])
 
-    with open('highres.csv', "a") as f:
+    with open('ETH_1s.csv', "a") as f:
         writer = csv.writer(f)
         writer.writerow(write)
 
