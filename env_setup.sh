51b6829c052dbb04a3b283a19f25892fb217d29c
mattsonthieme <mattson.thieme@gmail.com>
2020-03-10 19:35:13 -0500

    Add ccxt

--- env_setup.sh
+++ env_setup.sh
@@ -3,4 +3,5 @@ yes 'y' | conda create -n rltrade -c intel python=3
 source activate rltrade
 yes 'y' | conda install numpy
 yes 'y' | conda install pytorch torchvision -c pytorch
+pip install ccxt
 source deactivate
