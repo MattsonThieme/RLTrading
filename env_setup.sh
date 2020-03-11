729dfe6b4fcb4ca566d4df00165fce42b4231cf3
mattsonthieme <mattson.thieme@gmail.com>
2020-03-10 19:00:17 -0500

    Change env name

--- env_setup.sh
+++ env_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-yes 'y' | conda create -n rltest -c intel python=3
-source activate rltest
+yes 'y' | conda create -n rltrade -c intel python=3
+source activate rltrade
 yes 'y' | conda install numpy
 yes 'y' | conda install pytorch torchvision -c pytorch
 source deactivate
