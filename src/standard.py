99de44a2cc8df8ef61c4e13d61d1cd603d7f1117
mattsonthieme <mattson.thieme@gmail.com>
2020-03-18 12:38:31 -0500

    More updates - S&P is too volatile

--- src/standard.py
+++ src/standard.py
@@ -1,19 +1,18 @@
 # Standard strategy
-import configuration
+#import configuration
 import csv
 import numpy as np
 
 import matplotlib.pyplot as plt
 
-data_path = '../data/BTC-ETH-XLM-CVC_5s.csv'
-#data_path = '../data/crypto/ETH/ETH_1s_4.csv'
+#data_path = '../data/BTC-ETH-XLM-CVC_5s.csv'
+data_path = 'ETH_1s_4.csv'
+#data_path = 'VOO_copy.csv'
 
-assets = configuration.asset_names
+#assets = configuration.asset_names
 
-prices = [[], [], [], []]
-
-print(assets)
-
+'''
+# 5s gnn collection
 with open(data_path, 'r') as f:
     data = csv.reader(f)
     data = list(data)
@@ -24,63 +23,54 @@ with open(data_path, 'r') as f:
             prices[i].append(col)
 
 data = np.array(prices, dtype=float)
+'''
+
+# Standard 1s collection
+prices = [[]]
+with open(data_path, 'r') as f:
+    data = csv.reader(f)
+    data = list(data)
+    labels = data.pop(0)
+    print(labels)
+
+    for row in data:
+        prices[0].append(row[5])
+
+prices[0] = prices[0][:int(len(prices[0])*0.9)]
+
+indices = [i for i in range(0, len(prices[0]), 1)]
+
+length = len(prices[0])
+
+prices = np.array(prices[0], dtype=float)
+prices = prices[indices]
+
+
+data = np.array([list(prices)], dtype=float)
+
 
 
 def slope(last, current):
     return (current - last)
 
-def would_profit(bought_price, current):
-    raise NotImplementedError
-
 def profit(bought_price, sell_price, investment):
-    return sell_price*investment/bought_price - investment
+    fee = 0#0.00075
+
+    buy_cost = investment*fee
+    bought_shares = (investment - buy_cost)/bought_price
+    sold_revenue = bought_shares*sell_price
+    sold_cost = sold_revenue*fee
+    profit = sold_revenue - sold_cost - buy_cost - investment
+
+
+    #return sell_price*investment/bought_price - investment
+    return profit
+
 
 # Policy:
 
 # If the last slope was zero or negative, and this slope is positive, buy
 # If we've bought, and the slope goes to zero or negative, sell
-
-def iterate(data):
-    sold = None
-    bought = False
-    asset = "BTC"
-    asset = labels.index(asset+"-ask")
-    last_slope = slope(data[asset][0], data[asset][1])
-    last_ask = data[asset][0]
-    investment = 1000  # No fees
-
-    net = []
-    hold = []
-
-    for a, asset in enumerate(data):
-        net = []
-        hold = []
-        last_slope = slope(data[a][0], data[a][1])
-        last_ask = data[a][0]
-        for i, ask in enumerate(asset):
-            curr_slope = slope(last_ask, ask)
-
-            if bought == False:
-                if (last_slope <= 0) and (curr_slope > 0):
-                    bought = ask
-                    sold = False
-
-            if bought == True:
-                hold += 1
-
-            if sold == False:
-                if (last_slope >= 0) and (curr_slope < 0):
-                    net.append(profit(bought, ask, investment))
-                    bought = False
-                    sold = None
-
-            last_slope = curr_slope
-            last_ask = ask
-
-        print("\nAsset = ", assets[a])
-        print("Trades: ", len(net))
-        print("Avg profit:   $", sum(net)/len(net))
-        print("Total profit: $", sum(net))
 
 def iterate(data):
 
@@ -92,30 +82,42 @@ def iterate(data):
         sold = None
         bought = False
         investment = 1000  # No fees
-        
+
         net = []
         hold_time = 0
         hold = []
         last_slope = slope(asset[0], asset[1])
         last_ask = asset[0]
+        inv_track = []
+        profit_track = []
+        profit_trade = 0
 
         for i, ask in enumerate(asset):
+
+            inv_track.append(investment)
+            net.append(0)
+            profit_track.append(profit_trade)
 
             curr_slope = slope(last_ask, ask)
 
             if bought == False:
-                if (last_slope <= 0) and (curr_slope > 0):
+                if (last_slope >= 0) and (curr_slope > 0):
                     bought = ask
                     sold = False
+                    #net.append(0)
 
             if bought != False:
                 hold_time += 1
+                #net.append(0)
 
             if sold == False:
                 if (last_slope >= 0) and (curr_slope < 0):
-                    net.append(profit(bought, ask, investment))
+                    net[i] = profit(bought, ask, investment)
                     hold.append(hold_time)
                     investment += profit(bought, ask, investment)
+                    profit_trade += profit(bought, ask, investment)
+                    profit_track[i] = profit_trade
+                    inv_track[i] = investment
                     hold_time = 0
                     bought = False
                     sold = None
@@ -124,13 +126,32 @@ def iterate(data):
             last_slope = curr_slope
             last_ask = ask
 
-        plt.plot(net)
-        #plt.show()
-        print("\nAsset = {}, {} - {}".format(assets[a], asset[0], asset[len(asset)-1]))
-        print("Trades: ", len(net))
+        num_trades = np.count_nonzero(np.array(net))
+
+        print("\nAsset = {}, {} - {}, len: {}".format('VOO', asset[0], asset[len(asset)-1], len(asset)))
+        print("Trades: ", num_trades)
         print("Avg hold: ", sum(hold)/len(hold))
         print("Avg profit:   $", sum(net)/len(net))
         print("Total profit: $", sum(net))
+
+
+        fig, ax1 = plt.subplots()
+
+        color = 'tab:red'
+        ax1.set_xlabel('time (s)')
+        ax1.set_ylabel('exp', color=color)
+        ax1.plot(asset, color=color)
+        ax1.tick_params(axis='y', labelcolor=color)
+
+        ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+
+        color = 'tab:blue'
+        ax2.set_ylabel('sin', color=color)  # we already handled the x-label with ax1
+        ax2.plot(profit_track, color=color)
+        ax2.tick_params(axis='y', labelcolor=color)
+
+        fig.tight_layout()  # otherwise the right y-label is slightly clipped
+        plt.show()
 
 iterate(data)
 
